f7c4f7fc9ae683cb8e135e63d57d0e529ca8c42f
tomi <remioluwatomicephas@gmail.com>
2025-02-11 00:18:50 +0100

    Test CI workflow: Add an extra comment

--- api/routes/books.py
+++ api/routes/books.py
@@ -48,6 +48,7 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
 # comment to test ci-workflow 
+# comment to test ci-workflow 
 @router.get(
     "/{book_id}", response_model=Book, status_code=status.HTTP_200_OK
 )
