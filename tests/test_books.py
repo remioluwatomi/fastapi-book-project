f9b32030b8beebec1b83100555334d2dd40242d1
tomi <remioluwatomicephas@gmail.com>
2025-02-10 23:11:42 +0100

    Update(Test) - Updated test suite to check for non-existent book. asserted 404 status code and error message. Feat - Added continuous integration workflow.(Test job)

--- tests/test_books.py
+++ tests/test_books.py
@@ -50,3 +50,9 @@ def test_delete_book():
 
     response = client.get("/books/3")
     assert response.status_code == 404
+
+
+def test_get_nonexistent_book():
+    response = client.get("/books/999")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Book not found"}
